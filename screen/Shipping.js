971a6ccfb1d514176539fd136c1bd2d4d7ab2247
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-02-14 15:25:00 +0000

    file

---
+++ screen/Shipping.js
@@ -0,0 +1,100 @@
+// // import React from 'react';
+// // import { StatusBar } from 'expo-status-bar';
+// // import {Text,  View, TouchableOpacity } from 'react-native';
+// // import {FontAwesome} from 'react-native-vector-icons';
+
+// // export default function Shipping() {
+// //     return (
+// //         <View>
+// //             <View>
+// //                 <View>
+// //                     <View>
+// // <FontAwesome name='home'/>
+// // <Text>Home</Text>
+// //                     </View>
+// //                     <FontAwesome name= 'pencil'/>
+// //                 </View>
+// //                 <Text>Sunamamganji</Text>
+// //                 <Text>+971 50-123456</Text>
+// //                 <Text>Room#1-Ground Floor AL Najoun Building,24B Street, Dubai - United Arab Emirates</Text>
+                
+// //             </View>
+// //             <View>
+// //                 <View>
+// //                     <View>
+// // <FontAwesome name='home'/>
+// // <Text>Office</Text>
+// //                     </View>
+// //                     <FontAwesome name= 'pencil'/>
+// //                 </View>
+// //                 <Text>Sunamamganji</Text>
+// //                 <Text>+971 50-123456</Text>
+// //                 <Text>Room#1-Ground Floor AL Najoun Building,24B Street, Dubai - United Arab Emirates</Text>
+                
+// //             </View>
+// //             <View>
+// //                 <View>
+// //                     <Text>Add new shipping address</Text>
+// //                 </View>
+// //                 <Text>Personal Info</Text>
+// //                 <Text>Bill to the Home address</Text>
+// //                 <Text>01746247923</Text>
+// //                 <Text>quickrakibull@gmail.com</Text>
+// //             </View>
+
+// //             <TouchableOpacity style={{backgroundColor: '#add8e6',padding: 15,width: 300,borderRadius: 20,marginTop: 30, color: 'white',textAlign: 'center'}}>
+// //            <Text style={{ color: 'white', textAlign: 'center' }}>Continue</Text> 
+// //               </TouchableOpacity>
+// //             <StatusBar style='auto'/>
+// //         </View>
+// //     )
+// // }
+
+
+// import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { Text, View, TouchableOpacity } from 'react-native';
+// import { FontAwesome } from 'react-native-vector-icons';
+
+// export default function Shipping() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <View style={{ marginBottom: 20 }}>
+//         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+//           <FontAwesome name='home' />
+//           <Text>Home</Text>
+//         </View>
+//         <FontAwesome name='pencil' />
+//       </View>
+//       <Text>Sunamamganji</Text>
+//       <Text>+971 50-123456</Text>
+//       <Text>Room#1-Ground Floor AL Najoun Building, 24B Street, Dubai - United Arab Emirates</Text>
+      
+//       <View style={{ marginBottom: 20 }}>
+//         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+//           <FontAwesome name='home' />
+//           <Text>Office</Text>
+//         </View>  
+//         <FontAwesome name='pencil' />
+//       </View>
+//       <Text>Sunamamganji</Text>
+//       <Text>+971 50-123456</Text>
+//       <Text>Room#1-Ground Floor AL Najoun Building, 24B Street, Dubai - United Arab Emirates</Text>
+      
+//       <View>
+//         <View>
+//           <Text style={{marginTop:10,marginBottom:10}}> Add new shipping address</Text>
+//         </View>
+//         <Text style={{fontSize:15,fontWeight:'bold', marginTop:10, marginBottom:10}}>Personal Info</Text>
+//         <Text>Bill to the Home address</Text>
+//         <Text>01746247923</Text>
+//         <Text>quickrakibull@gmail.com</Text>
+//       </View>
+
+//       <TouchableOpacity style={{ backgroundColor: '#6ed8f5', padding: 30, width: 300, borderRadius: 20, marginTop: 30, color: 'white', textAlign: 'center' }}>
+//         <Text style={{ color: 'white', textAlign: 'center'  }}>Continue</Text>
+//       </TouchableOpacity>
+//       <StatusBar style='auto' />
+//     </View>
+//   );
+// }
