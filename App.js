223925f9b394629a662ff45494c60a7a1c76edba
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-02-16 21:27:20 +0000

    files

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { View,Text  } from 'react-native';
-// import Photo from './screen/photo.js';
+// import SignIn from './screen/SignIn.js';
+import React from 'react';
 import Login from './screen/Login.js'
+import Register from './screen/Register.js';
+import Home from './screen/Home.js'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 
 export default function App () {
   return (
-      <Login/>
+<NavigationContainer>
+<Stack.Navigator>
+        <Stack.Screen name='Login'  component={Login} />
+        <Stack.Screen name='Register'  component={Register}/>
+        <Stack.Screen name='Home'  component={Home} options={{headerShown : false}}/>
+      </Stack.Navigator>
+</NavigationContainer>
   );
 }
